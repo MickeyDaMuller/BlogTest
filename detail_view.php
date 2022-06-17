588cd789a5d25ad6233379a15e09ff431c9a6380
Michael Isijola <39952685+MickeyDaMuller@users.noreply.github.com>
2022-06-17 14:26:13 +0400

    Add files via upload

---
+++ detail_view.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+
+		<div>
+		<a href="home.php"><img src="https://via.placeholder.com/300.png/09f/fff" height="100px" width="100px" style="padding-right: 10px;"></a>Blog Name
+    </div>
+
+    <div>
+		<hr>
+		<a href="" style="text-decoration:none">Übersichht<a/>
+		 <b> | </b> 
+		 <a href="" style="text-decoration:none">[Neuer Eintrag]<a/> 
+		 <b> | </b>
+		 <a href="" style="text-decoration:none">Impressum<a/> 
+		 <a href="" style="text-decoration:none;float: right;">[Login/Logout]<a/>
+		<hr>
+	</div>
+
+	<div style="background-color:lightblue; padding: 10px;">
+
+		<p>Date</p>
+		<h1>Title</h1>
+		<img src="https://via.placeholder.com/300.png/09f/fff" width="100%" height="300px" >
+
+		<p>Lorem ipsum dolor sit amet. Et reiciendis culpa et harum magnam qui laborum dicta ex facilis ipsum qui autem dolores. Et provident velit ut alias sint ab libero nesciunt ex consequatur ducimus eos exercitationem tempora eos expedita obcaecati sed galisum vero! Aut exercitationem omnis sed ipsum voluptatibus ipsam voluptas et rerum dolorum a omnis explicabo.</p>
+
+		<p>Authhor: Michael</p>
+		
+
+	</div>
+	<p>1. Lorem ipsum dolor sit amet. Et reiciendis culpa et harum magnam qui laborum dicta ex facilis ipsum qui autem dolores. <span><a href=""><b>[X]</b></a></span></p> 
+
+	<div style="background-color:lightblue; padding: 10px;">
+		<center>
+			<form>
+
+				<span>Name * </span><input style="width:1000px" type="text" name="name" id="name">
+				<p id="nameError"></p>
+				<br>
+
+				<span>Mail  </span><input style="width:1000px" type="text" name="mail" id="mail">
+				<p id="mailError"></p>
+				<br>
+
+				<span>URL  </span><input style="width:1000px" type="text" name="url" id="url">
+				<p id="urlError"></p>
+				<br>
+
+				<span>Comment * </span><textarea rows="5" style="width:1000px" type="text" name="comment" id="comment"></textarea> 
+				<p id="mailError"></p>
+				<br>
+
+				<input type="submit" name="submit" value="Submit">
+
+			</form>
+		</center>
+
+	</div>
+</body>
+</html>
